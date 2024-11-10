4a374ee5b2b63f903d9e7a3f3f7e8bc32876e406
Bowie <invasao.burguer22@gmail.com>
2024-11-10 15:04:02 -0300

    Remover importação circular no arquivo pedidos.js

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Pedido = require("../models/Pedido");
-const pedidoRoutes = require("./routes/pedidos");
 
 router.post("/", async (req, res) => {
   const novoPedido = new Pedido(req.body);
